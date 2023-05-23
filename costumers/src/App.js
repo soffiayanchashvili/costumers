57a290dca1eb59fe18a2b70682c24c5d84c67c6b
soffiayanchashvili <119891979+soffiayanchashvili@users.noreply.github.com>
2023-05-23 23:53:04 +0400

    added styles and finished

--- costumers/src/App.js
+++ costumers/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUserData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setUsers(json);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>All Customers</h1>
       </header>
+      {users.length > 0 && (
+        <ul>
+          <div>
+            <p className="gray_p">Customer Name</p>
+            {users.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </div>
+          <div>
+            <p className="gray_p">Company</p>
+            {users.map((user) => (
+              <li key={user.name}>{user.company.name}</li>
+            ))}
+          </div>
+          <div>
+            <p className="gray_p">Phone Number</p>
+            {users.map((user) => (
+              <li key={user.id}>{user.phone}</li>
+            ))}
+          </div>
+          <div>
+            <p className="gray_p">Email</p>
+            {users.map((user) => (
+              <li key={user.id}>{user.email}</li>
+            ))}
+          </div>
+          <div>
+            <p className="gray_p">Street</p>
+            {users.map((user) => (
+              <li key={user.id}>{user.address.street}</li>
+            ))}
+          </div>
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default App;
